130fce2218ff38117ea3e78d590b47f103e450e6
Ajmal Razaq Bhatti <theajmalrzaq@gmail.com>
2024-06-29 09:47:09 -0700

    refactor: Update font URLs and paths for Gilroy and LemonMilk fonts

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const eventobj = {
   event_date: document.getElementById("event_date"),
-  event_poster: document.getElementById("event_poster"),
   event_name: document.getElementById("event_name"),
   event_description: document.getElementById("event_description"),
+  event_poster: document.getElementById("event_poster"),
   link_primary: document.getElementById("link_primary"),
   link_secondary: document.getElementById("link_secondary"),
 };
@@ -11,7 +11,10 @@ fetch("post.json")
   .then((data) => {
     let keysjson = Object.keys(data[0]);
     let objkeys = Object.keys(eventobj);
-    for (let i = 0; i < keysjson.length - 2; i++) {
+    for (let i = 0; i < keysjson.length - 3; i++) {
       eventobj[objkeys[i]].innerHTML = data[0][keysjson[i]];
     }
+    eventobj.event_poster.setAttribute("src", data[0].image_url);
+    eventobj.link_primary.setAttribute("href", data[0].link_primary);
+    eventobj.link_secondary.setAttribute("href", data[0].link_secondary);
   });
