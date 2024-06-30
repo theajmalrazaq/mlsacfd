a56425e460131908cfa233a8b55b3ca45e760a39
Ajmal Razaq Bhatti <theajmalrzaq@gmail.com>
2024-06-30 07:09:11 -0700

    refactor: Update event-info styling and add speaker, time, and venue details

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const eventobj = {
   event_date: document.getElementById("event_date"),
   event_name: document.getElementById("event_name"),
   event_description: document.getElementById("event_description"),
+  event_speaker: document.getElementById("event_speaker"),
+  event_time: document.getElementById("event_time"),
+  event_venue: document.getElementById("event_venue"),
   event_poster: document.getElementById("event_poster"),
   link_primary: document.getElementById("link_primary"),
   link_secondary: document.getElementById("link_secondary"),
@@ -12,7 +15,7 @@ fetch("post.json")
     let keysjson = Object.keys(data[0]);
     let objkeys = Object.keys(eventobj);
     for (let i = 0; i < keysjson.length - 3; i++) {
-      eventobj[objkeys[i]].innerHTML = data[0][keysjson[i]];
+      eventobj[objkeys[i]].innerHTML += data[0][keysjson[i]];
     }
     eventobj.link_primary.setAttribute("href", data[0].link_primary);
     eventobj.link_secondary.setAttribute("href", data[0].link_secondary);
