142bd817fdd10cea256cc3d735f8d26bbba63b9c
Ajmal Razaq Bhatti <theajmalrzaq@gmail.com>
2024-07-01 07:27:45 -0700

    updated

--- script.js
+++ script.js
@@ -21,3 +21,43 @@ fetch("post.json")
     eventobj.link_secondary.setAttribute("href", data[0].link_secondary);
     document.getElementById("event_form").value = `${data[0].title}`;
   });
+let teamCol = document.getElementById("team_col");
+fetch("team.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let classarr = [
+      "team_mem",
+      "is-flex",
+      "is-flex-direction-column",
+      "is-justify-content-end",
+      "p-5",
+    ];
+    for (let j = 0; j < data.length; j++) {
+      let team_mem = document.createElement("div");
+      for (let i = 0; i < classarr.length; i++) {
+        team_mem.classList.add(classarr[i]);
+      }
+      let namemem = document.createElement("h1");
+      let designation = document.createElement("h3");
+      namemem.classList.add("title");
+      namemem.classList.add("is-6");
+      namemem.innerHTML = data[j].name;
+      designation.innerHTML = data[j].designation;
+      namemem.style.display = "none";
+      designation.style.display = "none";
+      team_mem.appendChild(namemem);
+      team_mem.appendChild(designation);
+      teamCol.appendChild(team_mem);
+
+      // Add mouseover event listener to each team member div
+      team_mem.addEventListener("mouseover", function () {
+        changemem(team_mem, namemem, designation);
+      });
+    }
+  });
+
+function changemem(overmem, namemem, designation) {
+  overmem.style.width = "200px";
+  namemem.style.display = "";
+  designation.style.display = "";
+}
